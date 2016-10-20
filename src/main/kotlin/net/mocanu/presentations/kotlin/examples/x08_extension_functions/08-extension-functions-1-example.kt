566064ebc40d89a06f358557445fb2fe2054bb1b
Cristian Vasile Mocanu <cvmocanu@gmail.com>
2016-10-20 22:35:43 +0200

    add extension functions example

---
+++ src/main/kotlin/net/mocanu/presentations/kotlin/examples/x08_extension_functions/08-extension-functions-1-example.kt
@@ -0,0 +1,31 @@
+package net.mocanu.presentations.kotlin.examples.x08_extension_functions
+
+import net.mocanu.presentations.kotlin.examples.x08_extension_functions.my_package.removeSpaces
+import net.mocanu.presentations.kotlin.examples.x08_extension_functions.my_package.toTitleCase
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.text.WordUtils
+
+fun main(args: Array<String>) {
+    val name = "  jOHn dOE     "
+
+    // [ ] run & show what it prints
+    // [ ] show that is safe: explicit import (added automatically by code completion)
+    // [ ] show extension function declaration
+    println(
+            name.toTitleCase()
+                .removeSpaces()
+    )
+
+    // [ ] java-style => EF is just syntactic sugar for improved readability
+    println(
+            removeSpaces(
+                    toTitleCase(name)
+            )
+
+    )
+
+}
+
+private fun toTitleCase(text: String) = WordUtils.capitalizeFully(text)
+
+private fun removeSpaces(text: String) = StringUtils.remove(text, " ")
---
+++ src/main/kotlin/net/mocanu/presentations/kotlin/examples/x08_extension_functions/my_package/08-extension-functions-1-definition.kt
@@ -0,0 +1,8 @@
+package net.mocanu.presentations.kotlin.examples.x08_extension_functions.my_package
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.text.WordUtils
+
+fun String.toTitleCase() = WordUtils.capitalizeFully(this)
+
+fun String.removeSpaces() = StringUtils.remove(this, " ")
